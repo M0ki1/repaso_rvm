489d9cbd67103d0d066dbc57bb20e8cb38310947
webtech <webtech@webtech-debian.webtech>
2023-06-10 12:33:16 -0400

    feat: Validates name and last name in user controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  validate :first_name, presence :true
+  validate :last_name, presence :true
+
 end
